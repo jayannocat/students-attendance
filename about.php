31d6fbd077907a980c04ec499b14040d1bb52869
jayannocat <144760241+jayannocat@users.noreply.github.com>
2024-02-26 15:38:30 +0800

    Add files via upload

---
+++ about.php
@@ -0,0 +1,30 @@
+<?php
+include'header.php';
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Us</title>
+        
+</head>
+<body>
+    
+<br><br>
+<h1 align="center" style="color: lightgreen;">About Us</h1><br><br><br>
+    <p style="color: dimgray;" align="center">&nbsp; A Student Attendance website is an online platform or system that is specifically designed to manage and track student attendance. <br>
+It allows educational institutions, such as schools or universities, to efficiently record and monitor the attendance of their students. <br><br>
+&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;These websites typically provide features that enable administrators, teachers and even parents to easily access and update attendance records. <br>
+They may offer functionalities such as marking students present or absent, generating attendance reports, sending notifications to parents or <br> guardians,  and analyzing attendance data.
+<br><br> &nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Student Attendance websites can streamline the attendance tracking process, saving time and reducing paperwork for educational institutions. <br>
+They also provide a centralized and easily acccessible platform for all stakeholders involved in monitoring and managing student attendance.</p>
+        <br><br><br>
+        
+    
+</body>
+</html>
